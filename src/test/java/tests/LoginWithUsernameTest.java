90ea2c7fe4415a14669fddc4bcfac06cc275dbc0
Yulian <yulian.romanuk@itqc.ru>
2024-12-27 22:53:56 +0300

    added LoginWithUsernameTest

---
+++ src/test/java/tests/LoginWithUsernameTest.java
@@ -0,0 +1,33 @@
+package tests;
+
+import core.base.BaseTest;
+import core.pages.LoginPage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LoginWithUsernameTest extends BaseTest {
+    private static LoginPage loginPage;
+
+    @BeforeEach
+    public void prepare() {
+        open(baseUrl);
+        loginPage = new LoginPage();
+    }
+
+    @Test
+    public void LoginWithUsernameTest() {
+        //Попытка входа с {username}
+        loginPage.loginWithUsername("incorrectUser");
+
+        //Проверка наличия сообщения об ошибке
+        assertTrue(loginPage.isErrorMessageVisible(), "Сообщение об ошибке входа не отображается");
+        String expectedErrorMessage = "Введите пароль";
+        String actualErrorMessage = loginPage.getErrorMessageText();
+        assertEquals(expectedErrorMessage, actualErrorMessage, "Текст сообщения об ошибке не совпадает");
+
+    }
+}
