161b5dfc1b3756d54f8d21c11dff2c39e717c966
Yulian <yulian.romanuk@itqc.ru>
2025-01-10 23:38:33 +0300

    added AnonymRecovery by Phone & Email pages

---
+++ src/main/java/core/pages/RecoveryByEmail.java
@@ -0,0 +1,21 @@
+package core.pages;
+
+import com.codeborne.selenide.SelenideElement;
+import core.base.BasePage;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+public class RecoveryByEmail extends BasePage {
+    private SelenideElement codeButton = $("[data-l='t,submit']");
+
+    {
+        verifyPageElements();
+    }
+
+    @Step("Проверяем видимость кнопки на странице")
+    public void verifyPageElements() {
+        codeButton.shouldBe(visible);
+    }
+}
